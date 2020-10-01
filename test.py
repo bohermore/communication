a0889718db5476e61856e770372c400df0bed3b8
bohermore <68453255+bohermore@users.noreply.github.com>
2020-09-30 21:47:52 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,29 @@
+import numpy as np
+
+
 
 
 class Action(object):
     END_OF_PERIOD = 'End of period
+    STEAL = 'steal',
+    TOV = 'turnover
+
+PROB = {
+    Action.TOV: (0.07, 0.1),
+    Action.STL: (0.04, 0.08),
+
+}
 
 
+def get_ratio(num, den, stat):
+    param = PROB[stat]
+    delta = param.max - param.min
+    ratio = (num * param.scale)/ (num + den)
+    value = param.min + ratio * delta
+    return value
+    
+
+    
 def possession_step():
     
     # Intentional foul
@@ -34,4 +54,18 @@ def possession_step():
     # - - - DREB
 
 
-   
+def is_tov() {
+    prob_tov = get_ratio(this.def.tov_force, this.off.tov_avoid, Action.TOV)
+    if prob_tov > np.random.random():
+        pass
+    
+    prob_stl = get_ratio(this.def.tov_force, this.off.tov_avoid, Action.STL)
+    if prob_stl > np.random.random():
+        pass
+    # Steal 4-8
+    # help / one-on-one determines defensive side
+    # creator / passing determines offensive side
+    
+    # TOV
+    # Determined by creator / passing
+
