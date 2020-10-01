b4f368aeff02db3a534a7104d9e7e6f31c4fd914
bohermore <68453255+bohermore@users.noreply.github.com>
2020-09-30 20:50:34 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+
+
+class Action(object):
+    END_OF_PERIOD = 'End of period
+
+
+def possession_step():
+    
+    # Intentional foul
+    
+    # Intentionally run out clock
+    
+    # Accidentally run out clock
+    
+    # TOVs
+    # - Steal?
+    
+    # Non shooting fouls
+    # - - FT Made
+    # - - FT Missed
+    # - - - OREB
+    # - - - DREB
+    
+    
+    # Don't forget team rebounds
+    
+    # Shot
+    # - Made
+    # - Miss 
+    # - Shooting foul
+    # - - FT Made
+    # - - FT Missed
+    # - - - OREB
+    # - - - DREB
+
+
+   
